3886dcfd4df0a0682ad1d9799d9866d07882f593
Agustin <agsutinromano10@yahoo.com>
2022-06-25 12:55:16 -0400

    made some changes will continue

--- script.js
+++ script.js
@@ -1,24 +1,30 @@
-let myLibrary = [];
+// let myLibrary = [];
+let library;
 
 class Book {
   constructor(title, author, pages, isRead) {
-    (this.title = title),
-      (this.author = author),
-      (this.pages = pages),
-      (this.isRead = isRead);
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
   }
 }
 
+function addBookToLibrary() {
+  const newBook = new Book(
+    bookTitle.value,
+    authorName.value,
+    pagesOfBook.value
+  );
+  console.log(newBook);
+}
+
 const addBtn = document.querySelector(".add-btn");
-const bookName = document.querySelector("#book");
+const bookTitle = document.querySelector("#book-title");
 const authorName = document.querySelector("#author");
 const pagesOfBook = document.querySelector("#pages");
 const readBook = document.querySelector("#read");
 
 //Click the addBtn to submit bookName, authorName, pagesOfBook into the .main-page with the .book-updated styling
 
-function addBookToLibrary() {
-  addBtn.addEventListener("click", () => {
-    //the .main-page will update and display the book
-  });
-}
+addBookToLibrary();
